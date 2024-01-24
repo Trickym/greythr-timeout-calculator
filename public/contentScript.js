567d3c712ca7d6ddbf4079f05beb65c858b9b712
Prashant <pm21641@gmail.com>
2024-01-24 11:02:55 +0530

    intime stored in localstorage
    Storing the intime in local storage for todays date and emptying on start of each day so that the user will be able to view his/her punch out time after logging into the portal once.

--- public/contentScript.js
+++ public/contentScript.js
@@ -6,6 +6,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     try {
       let samePage = window.location.href?.includes("/attendance-info");
       let tableElements = document.querySelectorAll("table.ng-star-inserted");
+      let selectedDate = document.querySelectorAll(
+        ".ng-star-inserted.selected"
+      );
+      let presentMonthSelector = document.querySelectorAll(
+        ".gt-calendar-control"
+      );
       if (tableElements.length > 0) {
         let firstTableChild = tableElements[0];
         let tableBody = firstTableChild.querySelector("tbody");
@@ -19,8 +25,20 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
               if (paragraphTags.length > 0) {
                 let targetParagraph = paragraphTags[0];
                 let in_time = targetParagraph.textContent.trim();
+                let day = "";
+                let presentMonth = "";
+                if (selectedDate?.length > 0) {
+                  day =
+                    selectedDate[0].querySelectorAll(".cell-date")[0]
+                      ?.textContent;
+                }
+                if (presentMonthSelector?.length > 0) {
+                  presentMonth = presentMonthSelector[0]
+                    ?.querySelectorAll(".text-3")[0]
+                    ?.textContent?.trim();
+                }
                 if (in_time) {
-                  sendResponse({ in_time, samePage });
+                  sendResponse({ in_time, samePage, day, presentMonth });
                   return true; // Indicates the response will be sent asynchronously
                 }
               }
@@ -28,10 +46,22 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
           }
         }
       }
+      let day = "";
+      let presentMonth = "";
+      if (selectedDate?.length > 0) {
+        day = selectedDate[0].querySelectorAll(".cell-date")[0]?.textContent;
+      }
+      if (presentMonthSelector?.length > 0) {
+        presentMonth = presentMonthSelector[0]
+          ?.querySelectorAll(".text-3")[0]
+          ?.textContent?.trim();
+      }
       sendResponse({
         error: "Element not found or data extraction failed",
         samePage,
         notPunchData: samePage ? true : false,
+        day,
+        presentMonth,
       });
     } catch (error) {
       sendResponse({ error: error.message });
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Logo from "./greytip_logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 import moment from "moment";
+import axios from "axios";
 
 function App() {
   const [text, setText] = useState("Loading..."); // Initial state message
@@ -14,13 +15,41 @@ function App() {
   const [notGreyt, setNotGreyt] = useState(false);
   const [noPunchInTime, setNotPunchInTime] = useState(false);
   const [punchOutTime, setPunchOutTime] = useState();
+  const [date, setDate] = useState("");
+  const [localOn, setLocalOn] = useState(false);
+  function getMonthByName(monthName) {
+    const monthMoment = moment(monthName, "MMMM", "en");
+    let monthNumber = monthMoment.month() + 1;
+    if (monthNumber < 10) {
+      return `0${monthNumber}`;
+    }
+    return monthNumber;
+  }
   useEffect(() => {
+    if (localStorage.getItem("date")) {
+      let date = JSON.parse(localStorage.getItem("date"));
+      let selectedDateMoment = moment(date, "YYYY-MM-DD");
+      let todayDate = moment().startOf("day");
+      let isToday = selectedDateMoment.isSame(todayDate, "day");
+      if (!isToday) {
+        localStorage.clear();
+      }
+    }
     // Replace "YOUR_EXTENSION_ID" with your actual extension ID
     chrome.runtime.sendMessage(
       "cbkjckmlkdilmgahkaiaegigmfkfgbcg",
       { action: "fetchElement" },
       (response) => {
         console.log(response);
+        let month = getMonthByName(response?.presentMonth?.split(" ")[0]);
+        let year = response?.presentMonth?.split(" ")[1];
+        let day = response?.day;
+        let selectedDate = `${year}-${month}-${day}`;
+        setDate(selectedDate);
+        let selectedDateMoment = moment(selectedDate, "YYYY-MM-DD");
+        let todayDate = moment().startOf("day");
+        let isTodayDateSelected = selectedDateMoment.isSame(todayDate, "day");
+        console.log({ selectedDate, todayDate, isTodayDateSelected });
         if (response && response?.in_time) {
           let inputTime = response?.in_time;
           const parsedTime = moment(inputTime, "hh:mm:ss a");
@@ -29,10 +58,34 @@ function App() {
           const sevenThirtyPM = moment("07:30:00 pm", "hh:mm:ss a");
           const isGreaterThanSevenThirtyPM =
             calculatedTime.isAfter(sevenThirtyPM);
+
           if (isGreaterThanSevenThirtyPM) {
             setNotPunchInTime(true);
+            if (isTodayDateSelected) {
+              localStorage.setItem("date", JSON.stringify(selectedDate));
+              localStorage.setItem("no_punch_in", JSON.stringify(true));
+              localStorage.setItem(
+                "text",
+                JSON.stringify("Seems like you forgot to punch in!")
+              );
+              setLocalOn(true);
+            } else {
+              setLocalOn(false);
+            }
             setText("Seems like you forgot to punch in!");
           } else {
+            if (isTodayDateSelected) {
+              localStorage.setItem("date", JSON.stringify(selectedDate));
+              localStorage.setItem("no_punch_in", JSON.stringify(false));
+              localStorage.setItem("punch_out_time", JSON.stringify(result));
+              localStorage.setItem(
+                "text",
+                JSON.stringify("Your punch out time")
+              );
+              setLocalOn(true);
+            } else {
+              setLocalOn(false);
+            }
             setText(
               response?.in_time ? "Your punch out time" : "Data not found"
             );
@@ -42,14 +95,35 @@ function App() {
           console.error("Error:", response?.error || "No response");
           if (response?.samePage) {
             if (response?.notPunchData) {
+              if (isTodayDateSelected) {
+                localStorage.setItem("date", JSON.stringify(selectedDate));
+                localStorage.setItem("no_punch_in", JSON.stringify(true));
+                localStorage.setItem(
+                  "text",
+                  JSON.stringify("Seems like you forgot to punch in!")
+                );
+                setLocalOn(true);
+              } else {
+                setLocalOn(false);
+              }
               setNotPunchInTime(true);
               setText("Seems like you forgot to punch in!");
             }
           } else {
-            setNotGreyt(true);
-            setText(
-              "You need to route to the Attendance Info page in your greythr portal."
-            );
+            if (localStorage.getItem("text")) {
+              setPunchOutTime(
+                JSON.parse(localStorage.getItem("punch_out_time"))
+              );
+              setNotPunchInTime(
+                JSON.parse(localStorage.getItem("no_punch_in"))
+              );
+              setText(JSON.parse(localStorage.getItem("text")));
+            } else {
+              setNotGreyt(true);
+              setText(
+                "You need to route to the Attendance Info page in your greythr portal."
+              );
+            }
           }
         }
       }
@@ -74,6 +148,7 @@ function App() {
       {punchOutTime && (
         <>
           <h2>{text}</h2>
+          <h2>{moment(date).format("DD MMM YYYY")}</h2>
           <h3>{punchOutTime}</h3>
           {punchOutTime && <img src={busVector} alt="bus" />}
         </>
